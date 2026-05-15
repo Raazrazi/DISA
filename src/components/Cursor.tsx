2d61415bfab93508ca0eec694a825f3466fcf909
Raazrazi <i.students37dia@gmail.com>
2026-05-15 07:07:47 +0530

    Describe your changes here

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { useEffect,useRef } from 'react';
 import '../styles/CustomCursor.css'
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import '../styles/NavBar.css'
 import { useState,useEffect } from 'react'
 
